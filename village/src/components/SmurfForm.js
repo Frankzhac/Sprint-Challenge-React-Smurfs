3cd6b643eb26a3a7e8d2fa56fa60811a6b4b784a
Frankzhac <Frank.hemle@gmail.com>
2019-05-17 12:47:41 -0400

    add code to create a smurf

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'react';
+
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       name: '',
-      age: '',
+      age: null,
       height: ''
     };
   }
@@ -13,13 +15,27 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    const newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    }
+    axios
+      .post('http://localhost:3333/smurfs', newSmurf)
+      .then(res => {
+        console.log(res.data);
+       this.props.onSubmit(res.data);
 
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+      })
+      .catch(err => console.log(err));
   }
+
+  //   this.setState({
+  //     name: '',
+  //     age: '',
+  //     height: ''
+  //   });
+  // }
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
