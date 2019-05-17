97495a0e025979517d10c7de7aca6c9fdf017321
Frankzhac <Frank.hemle@gmail.com>
2019-05-17 12:27:57 -0400

    imported axios, smurf and constructed an axios resquest to get my smurf info

--- village/src/App.js
+++ village/src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import './App.css';
+import axios from "axios";
+
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Smurf from './components/Smurf';
+
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+      message: ""
     };
+    this.assembleVillage = this.assembleVillage.bind(this);
+  }
+
+  componentDidMount() {
+    this.assembleVillage();
+  }
+
+  assembleVillage() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => {this.setState({ smurfs: res.data, message: "New Blah blah" })
+      })
+      .catch(err => {
+        this.setState({
+          message: "Data fetching failed!"
+        });
+      });
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
